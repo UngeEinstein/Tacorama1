b72c8311efadd3684af9f99096f35924c6e7041a
hjaltipc <43811475+hjaltipc@users.noreply.github.com>
2018-10-23 09:25:47 +0200

    i

--- Site/Script/menu.js
+++ Site/Script/menu.js
@@ -1,11 +1,12 @@
 meny=["Booking", "Meny", "Våre restauranter"]
 targetElement= document.querySelector("#menu");
+
 for x in meny{
     createMenu(x);
 }
 def createMenu(x){
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(meny))
-  node.appendChild(li);
+  node.appendChild(p);
 
 }
