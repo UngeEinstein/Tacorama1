bb4d1837bafd65922b2cb4a37bc1ee9e87b51430
hjaltipc <43811475+hjaltipc@users.noreply.github.com>
2018-10-26 10:43:10 +0200

    links bro

--- Site/Script/menu.js
+++ Site/Script/menu.js
@@ -5,8 +5,16 @@ function addmenu(){
 
   const lagli = document.createElement("li");
   lagli.appendChild(document.createTextNode(meny[i]));
-  lagli.setAttribute("id",meny[i]);
   lagli.setAttribute("class","meny")
+  for (i in meny){
+    if (i = 1)
+      lagli.setAttribute("href", "/booking.html")
+    else if (i = 2)
+      lagli.setAttribute("href", "/meny.html")
+    else if (i = 3)
+      lagli.setAttribute("href", "/vare_restauranter.html")
+  }
+
   targetElement.appendChild(lagli);
 }
 for (i in meny){
