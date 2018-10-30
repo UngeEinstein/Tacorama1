8bd87d75dd34c5e803e45db8aff5f24c53f924fb
sepehrn0107 <43407185+sepehrn0107@users.noreply.github.com>
2018-10-30 17:31:16 +0100

    s

--- Site/Script/book.js
+++ Site/Script/book.js
@@ -5,25 +5,28 @@ dateElement.setAttribute("min", d.toJSON().split('T')[0])
 var div = document.getElementById('inputval');
 
 function show(value) { //funksjon til å åpne form når restaurant blir valgt
-    if (value !== "loc0") {
-    document.getElementById("form").style.display='initial';
-    }
-    else{
-    document.getElementById("form").style.display='none';
-    }
+  if (value !== "loc0") {
+    document.getElementById("form").style.display = 'initial';
+  } else {
+    document.getElementById("form").style.display = 'none';
   }
-  function myFunction() {
-    document.getElementById("submit").submit();
+}
+
+function myFunction() {
+  document.getElementById("submit").submit();
 
 }
+var form = document.getElementById("form");
+var isValidForm = form.checkValidity();
+
 function display() {
-  DispWin = window.open('','NewWin', 'toolbar=no,status=no,width=300,height=200,list-style=none')
-  message = "<b>   Bestillingen er fullført! </b>";
-  message += "<ul><li><b>Navn: </b>" + document.getElementById("Navn").value;
-  message += "<li><b>Etternavn: </b>" + document.getElementById("Enavn").value;
-  message += "<li><b>E-post: </b>" + document.getElementById("e-post").value;
-  message += "<li><b>Telefon: </b>" + document.getElementById("tlf").value;
-  message += "<li><b>Dato: </b>" + document.getElementById("minDate").value;
-  message += "<li><b>Tid: </b>" + document.getElementById("tid").value + "</ul>";
-  DispWin.document.write(message);
+    DispWin = window.open('', 'NewWin', 'toolbar=no,status=no,width=300,height=200,list-style=none')
+    message = "<b>   Bestillingen er fullført! </b>";
+    message += "<ul><li><b>Navn: </b>" + document.getElementById("Navn").value;
+    message += "<li><b>Etternavn: </b>" + document.getElementById("Enavn").value;
+    message += "<li><b>E-post: </b>" + document.getElementById("e-post").value;
+    message += "<li><b>Telefon: </b>" + document.getElementById("tlf").value;
+    message += "<li><b>Dato: </b>" + document.getElementById("minDate").value;
+    message += "<li><b>Tid: </b>" + document.getElementById("tid").value + "</ul>";
+    DispWin.document.write(message);
 }
