2b8576ae7de1d1d760cd8d262b6504f00d104101
hjaltipc <43811475+hjaltipc@users.noreply.github.com>
2018-10-30 17:22:40 +0100

    fiksa menybar 4real this time

--- Site/Script/navigationbar.js
+++ Site/Script/navigationbar.js
@@ -1,21 +1,25 @@
 menybar=["Booking", "Meny", "Våre restauranter"]
-
 function addmenu(){
   targetElement= document.querySelector("#navbar");
-
+  console.log(i)
   const laga = document.createElement("a");
   laga.appendChild(document.createTextNode(menybar[i]));
   laga.setAttribute("class","meny")
-  for (i in menybar){
-    if (i = 1)
-      laga.setAttribute("href", "./booking.html")
-    else if (i = 2)
-      laga.setAttribute("href", "./meny.html")
-    else if (i = 3)
-      laga.setAttribute("href", "./vare_restauranter.html")
+
+  gilink(laga)
+  targetElement.appendChild(laga);
+}
+function gilink(laga){
+  if (i == 0){
+    laga.setAttribute("href","./booking.html")
+  }
+  else if (i == 1){
+    laga.setAttribute("href", "./meny.html")
+  }
+  else if (i == 2){
+    laga.setAttribute("href", "./vare_restauranter.html")
   }
 
-  targetElement.appendChild(laga);
 }
 for (i in menybar){
     addmenu(i)
