e6c0cbe3a2052ee274b4da4d3d1d9fc6c82a414b
hjaltipc <43811475+hjaltipc@users.noreply.github.com>
2018-10-30 14:11:53 +0100

    meny css

--- Site/Script/menu.js
+++ Site/Script/menu.js
@@ -3,19 +3,19 @@ meny=["Booking", "Meny", "Våre restauranter"]
 function addmenu(){
   targetElement= document.querySelector("#menu");
 
-  const lagli = document.createElement("li");
-  lagli.appendChild(document.createTextNode(meny[i]));
-  lagli.setAttribute("class","meny")
+  const laga = document.createElement("a");
+  laga.appendChild(document.createTextNode(meny[i]));
+  laga.setAttribute("class","meny")
   for (i in meny){
     if (i = 1)
-      lagli.setAttribute("href", "/booking.html")
+      laga.setAttribute("href", "/booking.html")
     else if (i = 2)
-      lagli.setAttribute("href", "/meny.html")
+      laga.setAttribute("href", "/meny.html")
     else if (i = 3)
-      lagli.setAttribute("href", "/vare_restauranter.html")
+      laga.setAttribute("href", "/vare_restauranter.html")
   }
 
-  targetElement.appendChild(lagli);
+  targetElement.appendChild(laga);
 }
 for (i in meny){
     addmenu(i)
