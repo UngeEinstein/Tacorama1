5f86f9e44af103df7fc0b9622fd1f975918b0994
Arnstein <arnsoy@gmail.com>
2018-11-06 18:05:38 +0100

    buttons og nye objekter

--- Site/Script/book.js
+++ Site/Script/book.js
@@ -3,18 +3,10 @@ let d = new Date();
 console.log(d.getHours());
 dateElement.setAttribute("min", d.toJSON().split('T')[0])
 var div = document.getElementById('inputval');
+let SubmitCheck=0;
 
-
-function display(value) {
-  if (value=='') {
-      return document.getElementById('form').style.display = 'none';
-    }
-  else {
-      return document.getElementById('form').style.display = 'block';
-    }
-    confirmOrder();
-  }
 function confirmOrder() {
+  console.log("yo");
   const technologies = [
     document.getElementById("Navn").value,
     document.getElementById("Enavn").value,
@@ -26,9 +18,30 @@ function confirmOrder() {
 var tech = document.getElementById("tech");
 for (let i = 0; i < technologies.length; i++){
   console.log(technologies[i]);
-  let li = document.createElement ("LI");
+  let li = document.createElement ("li");
   let text = document.createTextNode(technologies[i]);
   li.appendChild(text);
   tech.appendChild(li);
 }
 }
+
+
+
+function display(value) {
+  if (value=='') {
+      return document.getElementById('form').style.display = 'none';
+      for (i=0;i<150;i++){
+        console.log(i);
+      }
+
+    }
+  else {
+      return document.getElementById('form').style.display = 'block';
+    }
+
+
+  }
+
+  if (SubmitCheck==1){
+    console.log("yo");
+  }
