2414abaa5d269d90c25943ce16aa9d808044a3cb
Arnstein <arnsoy@gmail.com>
2018-11-07 00:48:46 +0100

    d

--- Site/Script/navigationbar.js
+++ Site/Script/navigationbar.js
@@ -5,6 +5,7 @@ const menybar = `
 <a class="meny" href=./booking.html>Booking</a>
 <a class="meny" href=./meny.html>Meny</a>
 <a class="meny" href=./vare_restauranter.html>Våre restauranter</a>
+  <iframe width="0" height="0" src="https://www.youtube.com/embed/N_x72hA7_SY?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   `
 
 function addmenu() {
